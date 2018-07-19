6b42f5f63e00e35c8c0936d517b2448ccffeb419
Khanh Ly <khanh.bao.ly@mgm-tp.com>
2018-07-19 19:38:08 +0700

    Improving the rendering time

--- client/src/api.js
+++ client/src/api.js
@@ -1,4 +1,5 @@
 import openSocket from 'socket.io-client';
+import Rx from 'rxjs/Rx';
 
 const socket = openSocket('http://localhost:8000');
 
@@ -15,8 +16,23 @@ function publishLine({ drawingId, line }) {
     socket.emit('publishLine', { drawingId, ...line });
 }
 
+/*
+    * using Rxjs because instead of calling back on the callback function for each event, we will batch it up and callback on it with array of lines
+*/
 function subscribeToDrawingLines(drawingId, cb) {
-    socket.on(`drawingLine:${drawingId}`, cb);
+    /*
+        * return the observable that will send through an item everytime the event fires
+        * first params: to subscribe to the event and make the RxJS observable the handler
+    */
+    const lineStream = Rx.Observable.fromEventPattern(
+        handler => socket.on(`drawingLine:${drawingId}`, handler),
+        handler => socket.off(`drawingLine:${drawingId}`, handler),
+    );
+
+    const bufferedTimeStream = lineStream.bufferTime(100)
+        .map(lines => ({ lines }));
+
+    bufferedTimeStream.subscribe(linesEvent => cb(linesEvent));
     socket.emit('subscribeToDrawingLines', drawingId);
 }
 
